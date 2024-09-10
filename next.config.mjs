a6714f2f911138de8cdd439876ca2e4bff70b688
chezbro <ericchesbrough@gmail.com>
2024-09-10 12:56:34 -0700

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/tripshaman',
-    async headers() {
-      return [
-        {
-          source: '/:path*',
-          headers: [
-            { key: 'X-Frame-Options', value: 'ALLOWALL' },
-          ],
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;    
+  basePath: process.env.NODE_ENV === 'production' ? '/tripshaman' : '',
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'X-Frame-Options', value: 'ALLOWALL' },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
