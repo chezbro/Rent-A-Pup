556edbbbb1ded8f6aca7ae5961b61919ed9345d5
chezbro <ericchesbrough@gmail.com>
2024-09-10 13:21:59 -0700

    links updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: process.env.NODE_ENV === 'production' ? '/tripshaman' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/tripshaman' : '',
   async headers() {
     return [
       {
