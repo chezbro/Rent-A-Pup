49fc24a03e5304390995f83ed1cdab5713b033e2
chezbro <ericchesbrough@gmail.com>
2024-10-09 10:27:18 -0700

    next config now default

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'X-Frame-Options', value: 'ALLOWALL' },
-        ],
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/rent-a-pup/:path*',
-        destination: '/:path*',
-      },
-    ];
-  },
+  // Add any necessary configuration options here
 };
 
 export default nextConfig;
